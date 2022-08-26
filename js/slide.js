11b87f7253be2e98f8e8e7f89e52ae978b1354ea
denis rodrigues <denisrodrigues.ita@gmail.com>
2022-08-26 14:51:06 -0300

    esquele dos eventos

---
+++ js/slide.js
@@ -0,0 +1,37 @@
+export default class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    this.wrapper.addEventListener('mousemove')
+  }
+
+  addSlideEvents() {
+    this.wrapper.addSlideEvents('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+  }
+
+  onMove(event) {
+
+  }
+
+  onEnd(event) {
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.onMove();
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
